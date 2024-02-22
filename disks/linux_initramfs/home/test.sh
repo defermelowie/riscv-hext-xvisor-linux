a0227e08a61aebf9eb99bd6d6d916a83121e1c6e
Lowie Deferme <lowie.deferme@kuleuven.be>
2024-02-22 09:04:46 +0100

    wip: update linux test script to contain some file system operations
    Co-authored-by: Lowie Deferme <lowie.deferme@kuleuven.be>
    Co-authored-by: Lowie Deferme <lowiecode@gmail.com>

--- disks/linux_initramfs/home/test.sh
+++ disks/linux_initramfs/home/test.sh
@@ -3,6 +3,16 @@
 echo "[test] Show file structure"
 ls -alh /
 
+echo "[test] Try filesystem operations"
+cd /tmp
+pwd
+ls -alh
+echo "hello world" > newfile.txt
+mkdir newfolder
+cp newfile.txt newerfile.txt
+hexdump -C newerfile.txt
+ls -alh
+
 echo "[test] Busybox help"
 busybox --help
 
